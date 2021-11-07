8bacdf49ff83c63cbef2988f82ade04ae13d9b24
tianwen <tianwenw.vk@gmail.com>
2021-11-08 03:58:35 +0800

    update config

--- scripts/eth/start_eth_node.sh
+++ scripts/eth/start_eth_node.sh
@@ -9,5 +9,5 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 ${ETH_BIN}/geth --datadir=${ETH_DATA}_${shardID} --rpc --rpcport "8000" --syncmode "full" --cache 4096 --gasprice 0 --networkid 10001 --nodiscover --unlock 0 #--password <(echo -n "") js <(echo 'console.log(admin.nodeInfo.enode);') 
 #--mine --minerthreads 1 --unlock 0 console 2>>geth.log
-# --cache 4096
-#${ETH_BIN}/geth --datadir=${ETH_DATA}_${shardID} --rpc --rpcport \"$((8000 + ${c}))\" --syncmode "full" --gasprice 0 --networkid 10001 --nodiscover --unlock 0 #--password <(echo -n "") js <(echo 'console.log(admin.nodeInfo.enode);') 
+# 
+#${ETH_BIN}/geth --datadir=${ETH_DATA}_${shardID} --rpc --rpcport \"$((8000 + ${c}))\" --syncmode "full" --cache 40960 --gasprice 0 --networkid 10001 --nodiscover --unlock 0 --mine --minerthreads 1 --unlock 0 console 2>>geth.log
