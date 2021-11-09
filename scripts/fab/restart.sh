790dd947c9193dd249ea7b899e3cf7ff5660988e
tianwen <tianwenw.vk@gmail.com>
2021-11-09 13:23:19 +0800

    fab scripts

--- scripts/fab/restart.sh
+++ scripts/fab/restart.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
-
+# fabric repository: run script to deploy kvstore chaincode
 #./network.sh -h
 ./network.sh down
 ./network.sh up createChannel
 docker ps -a
-./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
 
 ./network.sh createChannel -c kvchannel
 ./network.sh deployCC -c kvchannel -ccn kvstore -ccp ../kvstore/chaincode-go -ccl go
