e70390998f1c48d49fef3e12cb016c7241b2bf72
tianwen <tianwenw.vk@gmail.com>
2021-11-13 05:05:02 +0800

    experiments scripts

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -6,15 +6,15 @@ bestnodes=${1:-16}
 bestclients=${2:-16}
 dir=$(pwd)
 # Experiment 1
-${dir}/scripts/experiment1.sh 4 > experiment1.log 2>&1
+${dir}/scripts/experiments/experiment1.sh 4 > experiment1.log 2>&1
 
 
 # Experiment 2
-${dir}/scripts/experiment2.sh ${bestclients} > experiment2.log 2>&1
+${dir}/scripts/experiments/experiment2.sh ${bestclients} > experiment2.log 2>&1
 
 
 # Experiment 4
-${dir}/scripts/experiment3.sh ${bestnodes} ${bestclients} > experiment4.log 2>&1
+${dir}/scripts/experiments/experiment3.sh ${bestnodes} ${bestclients} > experiment4.log 2>&1
 
 
 
