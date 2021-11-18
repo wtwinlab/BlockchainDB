568ba8e9fc75ae57fe7eb60c46559b5562f38b9a
tianwen <tianwenw.vk@gmail.com>
2021-11-18 20:04:00 +0800

    multinode & Dockerfile

---
+++ multinode/kill_containers_blockchaindb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# kill and remove docker containes
+#
+
+IMGNAME="blockchaindb"
+PREFIX="blockchaindb"
+
+idx=1
+for id in `docker ps | grep $PREFIX | cut -d ' ' -f 1`; do
+	echo "$PREFIX$idx"
+	idx=$(($idx+1))
+	docker kill $id
+	docker rm $id
+	echo ''
+done
+
+# idx=1
+# for id in `docker ps | grep "redis-shard" | cut -d ' ' -f 1`; do
+# 	echo "redis-shard$idx"
+# 	idx=$(($idx+1))
+# 	docker kill $id
+# 	docker rm $id
+# 	echo ''
+# done
